3dabe3f14c912d7be46be9ae1d5aa378adc6d516
Dolapo-mosuro <mosurodolapo@gmail.com>
2022-12-12 08:08:32 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x;
+
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
