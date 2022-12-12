d91632d6600a913d11eb060198781596aba392c2
Dolapo-mosuro <mosurodolapo@gmail.com>
2022-12-12 08:47:29 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <studio.h>
+
+/**
+ *main - prints alphabets in lowercase, and in uppercase
+ *followed by a new line
+ *Return: 0 Always
+ */
+int main(void)
+{
+int x;
+
+for (x = 'a'; x <= 'z'; x++)
+putchar(x);
+for (x = 'A'; x <= 'Z'; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
