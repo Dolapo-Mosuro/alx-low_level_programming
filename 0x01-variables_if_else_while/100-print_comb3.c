adc9e2eae0ac638cc9da6df4a4cef5480de1b766
Dolapo-mosuro <mosurodolapo@gmail.com>
2022-12-12 19:09:34 -0800

    all three advanced task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of two digits
- * Return: ALways 0 (Success)
+ * main - Prints
+ *
+ * Return: Always 0
  */
+
 int main(void)
 {
-int n, m;
-
-for (n = 48; n <= 56; n++)
+int digit1, digit2;
+for (digit1 = 0; digit1 < 9; digit1++)
 {
-for (m = 49; m <= 57; m++)
-if (m > n)
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
-putchar(n);
-putchar(m);
-if (n != 56 || m != 57)
-{
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
+if (digit1 == 8 && digit2 == 9)
+continue;
 putchar(',');
 putchar(' ');
-}
-}
 }
 }
 putchar('\n');
