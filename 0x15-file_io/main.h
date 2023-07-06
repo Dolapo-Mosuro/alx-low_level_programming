a2bf5f004c5708fe5a97183695dd0da5ed5368c3
dolapo-mosuro <mosurodolapo@gmail.com>
2023-07-06 10:40:30 +0100

    deleted files for new cohort

--- 0x15-file_io/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef IO_
-#define IO_
-int _putchar(char c);
-int _strlen(const char *s);
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
-#endif
