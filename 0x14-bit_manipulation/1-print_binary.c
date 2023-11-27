044a6e787586c4fe2baf6585d3743f5cf62ffc00
Dolapo Mosuro <mosurodolapo@gmail.com>
2023-11-27 13:29:25 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,17 +1,48 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_binary - converts unsigned int to binary
- * @n: unsigned int
- * Return: binary
+ * _pow - function calculate the (base ^ power)
+ * @base: base of the exponent
+ * @power: the power of the exponent
+ *
+ * Return: value of (base ^ power)
+ */
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+unsigned long int num;
+unsigned int a;
+
+num = 1;
+for (a = 1; a <= power; a++)
+num *= base;
+return (num);
+}
+
+/**
+ * print_binary - print a number in binary notations
+ * @n: number is to print
+ *
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
-if (n > 1)
-    /*right shift operator*/
-    /*shifts all bits towards right by certain numbf 1 bits*/
-print_binary(n >> 1);
+unsigned long int divisor, check;
+char flag;
 
-_putchar((n & 1) + '0');
+flag = 0;
+divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+while (divisor != 0)
+{
+check = n & divisor;
+if (check == divisor)
+{
+flag = 1;
+_putchar('1');
+}
+else if (flag == 1 || divisor == 1)
+{
+_putchar('0');
+}
+divisor >>= 1;
+}
 }
